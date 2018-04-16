2518dd324f435f586632711518ab20ae4ab10b88
Jairo <jairo.viciana@accenture.com>
2018-04-16 10:13:47 +0200

    no favicon

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,21 @@
-const path = require('path');
-const favicon = require('serve-favicon');
-const compress = require('compression');
-const cors = require('cors');
-const helmet = require('helmet');
-const logger = require('winston');
+const path = require("path");
+const favicon = require("serve-favicon");
+const compress = require("compression");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("winston");
 
-const feathers = require('@feathersjs/feathers');
-const configuration = require('@feathersjs/configuration');
-const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
+const feathers = require("@feathersjs/feathers");
+const configuration = require("@feathersjs/configuration");
+const express = require("@feathersjs/express");
+const socketio = require("@feathersjs/socketio");
 
+const middleware = require("./middleware");
+const services = require("./services");
+const appHooks = require("./app.hooks");
+const channels = require("./channels");
 
-const middleware = require('./middleware');
-const services = require('./services');
-const appHooks = require('./app.hooks');
-const channels = require('./channels');
-
-const sequelize = require('./sequelize');
+const sequelize = require("./sequelize");
 
 const app = express(feathers());
 
@@ -28,9 +27,9 @@ app.use(helmet());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+//app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
-app.use('/', express.static(app.get('public')));
+app.use("/", express.static(app.get("public")));
 
 // Set up Plugins and providers
 app.configure(express.rest());
