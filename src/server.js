4efae8176e19637fcda38539eb2d0bb471186f03
Jairo <jairo.viciana@accenture.com>
2018-04-20 11:05:19 +0200

    up

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ let socket;
 if (window.location.hostname === "localhost") {
   socket = io("localhost:3030");
 } else {
-  socket = io();
+  socket = io("suncork.net:3030");
 }
 const server = feathers();
 server.configure(socketio(socket, { timeout: 25000 }));
