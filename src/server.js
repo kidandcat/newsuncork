36d8b3b909332b8500a736701f7e52ffc0dc9740
Jairo <jairo.viciana@accenture.com>
2018-04-20 11:19:34 +0200

    up

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import io from "socket.io-client";
 let socket;
 if (window.location.hostname === "localhost") {
   socket = io("localhost:3030");
+} else if (window.location.hostname === "10.114.146.70") {
+  socket = io("10.114.146.70:3030");
 } else {
   socket = io();
 }
